7be15da89be573e73f394f27ede730bf31f81244
Aashish Illa <aashish.illa@oracle.com>
2024-11-15 05:03:17 +0530

    Added meteors effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Meteors from "@/components/ui/meteors";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white p-8">
-      <h1 className="text-3xl sm:text-5xl font-bold">
-        We will meet soon
-      </h1>
+    <div className="flex items-center justify-center w-full min-h-screen bg-black">
+      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+        <Meteors number={30} />
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-r from-purple-300 via-blue-500 to-pink-300 bg-clip-text text-center text-8xl font-bold leading-relaxed text-transparent">
+          Coming Soon
+        </span>
+      </div>
     </div>
   );
 }
